753ad2609feb24f75ba94120e60efa213c328025
wuminye <huanlekf@163.com>
2015-02-09 12:32:20 +0800

    Model 3 测试
    同model1 , 另将区域分成3块

--- calcweight.m
+++ calcweight.m
@@ -1,5 +1,5 @@
 function res = calcweight(x1,x2,orgin)
-  %res =  sqrt(sum((x1-x2).^2));
+  res =  sqrt(sum((x1-x2).^2));
   orgin = [0 ;0] ;
-  res = ( sqrt(sum((x1-orgin).^2))+(sqrt(sum((x2-orgin).^2))))*(sqrt(sum((x1-x2).^2)))/2;
+  %res = ( sqrt(sum((x1-orgin).^2))+(sqrt(sum((x2-orgin).^2))))*(sqrt(sum((x1-x2).^2)))/2;
 end
--- myrun.m
+++ myrun.m
@@ -3,7 +3,7 @@ function [ cost , res ] = myrun( scale,search_radius,theta,R)
 %   Detailed explanation goes here
 
 fprintf('========Begin========\n');
-cost = [0 0];
+
 [rs,u,v,z] = plotBoundary(theta,scale); %获取边界
 res.u = u;
 res.v = v;
@@ -31,49 +31,53 @@ for p = 14:14
     line([rs(1,index) rs(1,index)+3*scale],[rs(2,index) rs(2,index)+k*3*scale]);
     line([rs(1,index) rs(1,index)-3*scale],[rs(2,index) rs(2,index)-k*3*scale]);
 
-    [ circle ] = GetCircle(rs,rs(:,index),k,search_radius,theta);
+    [ circle  ] = GetCircle(rs,rs(:,index),k,search_radius,theta);
 
     
     
-    di = sqrt(sum((circle-repmat([0;0],1,size(circle,2))).^2));
-    [c,ii] = min(di);
-    
-    %选起点
-    %ii = 1;
-    tv = circle(:,ii);
-    circle(:,ii) = circle(:,1);
-    circle(:,1) = tv;
-    plot(circle(1,1),circle(2,1),'b*');
-    
-    res.circle = circle;
-    
-    axis([-scale,scale,-scale,scale]);
+    [myc ks] = divideCircle( circle , rs(:,index) ,k,scale , 3 );
+    res.circles = myc;
+    res.ks = ks;
+    for h = 1 : length(myc)  
+        circle = res.circles{h};
+        di = sqrt(sum((circle-repmat([0;0],1,size(circle,2))).^2));
+        [c,ii] = min(di);
 
-    [ index ] = Prime( circle );
-    res.old_index = index;
-    cost(1) = CalcCost( circle , index ) ;
-    [ index ] = OptimAlgorithm( circle , index );
-  
-    res.index = index;
-    
-    cost(2) = CalcCost( circle , index ) ;%+ calcweight(circle(:,end),circle(:,1),circle(:,1)) ;
-    plot(circle(1,index),circle(2,index),'c-.','LineWidth',1.6);
+        %选起点
+        ii = 1;
+        tv = circle(:,ii);
+        circle(:,ii) = circle(:,1);
+        circle(:,1) = tv;
+        plot(circle(1,1),circle(2,1),'b*');
 
-    cost(3) = 0;
-    %tt = 0;
-    su = 0;
-    for i = 1 : size(circle,2)
-        pos = find(index==i);
-        tem = index(1:pos);
-       %  cost(3) =cost(3) + CalcCost( circle , tem );
-        cost(3) =cost(3) + CalcCost( circle , tem )*calcGuass(circle(:,pos),R);
-        su = su + calcGuass(circle(:,pos),R);
+        res.circle = circle;
+
+        axis([-scale,scale,-scale,scale]);
+
+        [ index ] = Prime( circle );
+        res.old_index{h} = index;
+        cost{h}(1) = CalcCost( circle , index ) ;
+        [ index ] = OptimAlgorithm( circle , index );
+
+        res.index{h} = index;
+
+        cost{h}(2) = CalcCost( circle , index ) ;%+ calcweight(circle(:,end),circle(:,1),circle(:,1)) ;
+        plot(circle(1,index),circle(2,index),'c-.','LineWidth',1.6);
+
+        cost{h}(3) = 0;
+        %su = 0;
+        for i = 1 : size(circle,2)
+            pos = find(index==i);
+            tem = index(1:pos);
+             cost{h}(3) =cost{h}(3) + CalcCost( circle , tem );
+           % cost(3) =cost(3) + CalcCost( circle , tem )*calcGuass(circle(:,pos),R);
+            %su = su + calcGuass(circle(:,pos),R);
+        end
+       % cost(3) = cost(3) / su;
+        cost{h}(3) = cost{h}(3) / size(circle,2);
+        %fprintf('%.5f\n',tt/size(circle,2));
+        %cost = cost + tt;
     end
-    cost(3) = cost(3) / su;
-    %cost(3) = cost(3) / size(circle,2);
-    %fprintf('%.5f\n',tt/size(circle,2));
-    %cost = cost + tt;
-    
 end
 end
 
